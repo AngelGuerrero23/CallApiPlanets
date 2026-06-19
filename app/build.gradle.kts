723da9ddbaef5140c1c4ca2ea0fc0c05090b4d9e
angel <angel_guerrero1@ucne.edu.do>
2026-06-18 23:27:41 -0400

    Ajusta versiones de dependencias y plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt.android)
 }
 
 android {
     namespace = "com.android.callapiplanets"
-    compileSdk =37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.android.callapiplanets"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -31,6 +33,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
